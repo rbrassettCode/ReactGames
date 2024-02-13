209089ebcf4c3032e26bb1b2b97fc6cd629a35f8
rbrassettCode <rbrassett@outlook.com>
2024-02-13 16:31:56 -0500

    Made tictactoe game blank spaces to be null instead of ' '

--- my-games/src/components/TicTacToe.js
+++ my-games/src/components/TicTacToe.js
@@ -1,8 +1,8 @@
 import React , {useState} from "react";
 
-const Square = ({value, onSquareClick}) => {
+const Square = ({value, onSquareClick, color}) => {
     return (
-        <button className="square" onClick={onSquareClick}>
+        <button className="p-4 border border-black " onClick={onSquareClick}>
             {value}
         </button>
     )
@@ -10,14 +10,14 @@ const Square = ({value, onSquareClick}) => {
 
 const TicTacToe = () => {
 
-    const [squares, setSquares] = useState(Array(9).fill('B'));
+    const [squares, setSquares] = useState(Array(9).fill(null));
     const [turn, setTurn] = useState('X');
     const [winner, setWinner] = useState(null);
 
     const handleClick = (index) => {
         //check winner
         if(winner ===  null) {
-            if(squares[index] === 'B') {
+            if(squares[index] === null) {
                 const newSquares = [...squares];
                 newSquares[index] = turn;
                 setTurn((turn === 'X' ? 'O' : 'X'));
@@ -28,12 +28,7 @@ const TicTacToe = () => {
     }
 
     const calculateWinner = (squaresToWin) => {
-        //Check for draw
-        if(!squaresToWin.includes('B')){
-            setWinner('Game Ended in a Draw');
-            console.log('draw found');
-            return 
-        }
+        
         // Define winning combinations (indices in the array)
         const winningCombinations = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
@@ -52,6 +47,13 @@ const TicTacToe = () => {
                     setWinner("Winner is: " + squaresToWin[a]);
                 }
             }
+            
+        }
+        //Check for draw
+        if(!squaresToWin.includes(null)){
+            setWinner('Game Ended in a Draw');
+            console.log('draw found');
+            return 
         }
 
         // No winner yet
@@ -61,27 +63,24 @@ const TicTacToe = () => {
     const handleRestart = () => {
         setTurn('X');
         setWinner(null);
-        setSquares(Array(9).fill('B'));
+        setSquares(Array(9).fill(null));
     }
 
     return(
         <div>
             {winner && <h2>{winner}</h2>}
-
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>                
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>                
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>                
+            <div className="flex justify-center max-w max-h">
+                <div className="text-xl  grid grid-cols-3 gap-0 p-0 m-0">
+                        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>                
+                        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>                
+                        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>                
+                </div>
             </div>
 
             <button onClick={handleRestart}>Restart</button>
